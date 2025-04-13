85b36682c2c1c17abbf7f8143dad4c7d0cefd975
ahmedooo1 <94185300+ahmedooo1@users.noreply.github.com>
2025-04-13 16:05:29 +0200

    Refactor: --1.5.1--Remove current_user dependency from read_users and read_user endpoints

--- services/user_service/app/api/endpoints/users.py
+++ services/user_service/app/api/endpoints/users.py
@@ -78,12 +78,12 @@ def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
     return create_user(db=db, user=user)
 
 @router.get("/users/", response_model=List[UserResponse])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), current_user: UserResponse = Depends(get_current_user)):
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = get_users(db, skip=skip, limit=limit)
     return users
 
 @router.get("/users/{user_id}", response_model=UserResponse)
-def read_user(user_id: int, db: Session = Depends(get_db), current_user: UserResponse = Depends(get_current_user)):
+def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="Utilisateur non trouvé")
